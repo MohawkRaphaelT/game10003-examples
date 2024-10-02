517c47d28d6272069efbe2775923664b279f76e2
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-10-02 18:10:17 -0400

    Add frog encapsulated

---
+++ pages/encapsulation/Frogs (Encapsulation)/Frog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+namespace Game10003;
+
+public class Frog
+{
+    public Color skin;
+    public Color cheek;
+    public float x;
+    public float y;
+
+    public void Render()
+    {
+        Draw.LineSize = 0;
+        // Body and eyes (green)
+        Draw.FillColor = skin;
+        Draw.Circle(x - 25, y - 70, 23);
+        Draw.Circle(x + 25, y - 70, 23);
+        Draw.Capsule(x - 50, y - 40, x + 50, y - 40, 25);
+        // Feet
+        Draw.Square(x - 20, y - 15, 15); // L
+        Draw.Square(x + 05, y - 15, 15); // L
+        Draw.Rectangle(x - 40, y - 12, 32, 12); // L
+        Draw.Rectangle(x + 05, y - 12, 32, 12); // R
+        Draw.Circle(x - 40, y - 6, 6); // L
+        Draw.Circle(x + 39, y - 6, 6); // R
+        // Cheeks
+        Draw.FillColor = cheek;
+        Draw.Circle(x - 50, y - 40, 20);
+        Draw.Circle(x + 50, y - 40, 20);
+        // Eyes
+        Draw.FillColor = Color.White;
+        Draw.Circle(x - 25, y - 70, 18);
+        Draw.Circle(x + 25, y - 70, 18);
+        Draw.FillColor = Color.Black;
+        Draw.Circle(x - 25, y - 70, 10);
+        Draw.Circle(x + 25, y - 70, 10);
+        // Mouth
+        Draw.LineSize = 3;
+        Draw.LineColor = Color.Black;
+        Draw.PolyLine(x - 15, y - 40, x, y - 25, x + 15, y - 40);
+    }
+}
---
+++ pages/encapsulation/Frogs (Encapsulation)/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace Game10003;
+
+public class Game
+{
+    // Place your variables here:
+    Color bg = new Color(0, 161, 224);
+    Color frogGreen = new Color(140, 240, 110);
+    Color frogPink = new Color(255, 170, 150);
+
+    Frog frog;
+
+    public void Setup()
+    {
+        Window.SetTitle("Frog (Encapsulated)");
+        Window.SetSize(800, 600);
+
+        // Initialize class
+        frog = new Frog();
+        frog.skin = frogGreen;
+        frog.cheek = frogPink;
+    }
+
+    public void Update()
+    {
+        // Reset background
+        Window.ClearBackground(bg);
+
+        // Assign to frog's position
+        frog.x = Input.GetMouseX();
+        frog.y = Input.GetMouseY();
+        // Call frog function to draw it with it's internal variables
+        frog.Render();
+    }
+}
